42e99d80e48d42c10b55d7a8808221fa056ffd6e
leticea <nascimento.leticeah@gmail.com>
2024-03-02 13:44:54 -0300

    installing react helmet async

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./global.css";
 
 import { RouterProvider } from "react-router-dom";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import { router } from "./routes";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
