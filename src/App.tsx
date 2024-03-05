2ce8058a6ec9cae67074db82fb0a18b629a3cf12
leticea <nascimento.leticeah@gmail.com>
2024-03-05 18:44:06 -0300

    feat(sign-in.tsx): using sonner

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./global.css";
 
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
+import { Toaster } from "sonner";
 import { router } from "./routes";
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster />
       <RouterProvider router={router} />
     </HelmetProvider>
   );
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -23,6 +24,8 @@ export function SignIn() {
     console.log(data);
 
     await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    toast.success("Enviamos um link de autenticação para seu e-mail.");
   }
 
   return (
