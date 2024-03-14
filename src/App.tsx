e3262d81da08b87a5175e688a048183ab3912357
leticea <nascimento.leticeah@gmail.com>
2024-03-14 19:40:35 -0300

    creating theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { Toaster } from "sonner";
 import { router } from "./routes";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
