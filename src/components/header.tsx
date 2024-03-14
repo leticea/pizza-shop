84e49601c4c41f9cc9e6bae92416d66a2b4c62a5
leticea <nascimento.leticeah@gmail.com>
2024-03-14 19:14:57 -0300

    feat(header.tsx): creating header component

---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import { Home, Pizza, UtensilsCrossed } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Pizza className="h-6 w-6" />
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <Link to="/">
+            <Home className="h-4 w-4" />
+            Início
+          </Link>
+          <Link to="/orders">
+            <UtensilsCrossed className="h-4 w-4" />
+            Pedidos
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
+}
