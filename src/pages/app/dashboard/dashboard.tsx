699338f73a6f30e3f4731cf2bb7a0c23e5eed51f
leticea <nascimento.leticeah@gmail.com>
2024-03-21 16:36:57 -0300

    feat(dashboard.tsx): creating the cards

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,6 +1,8 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { DollarSign } from "lucide-react";
 import { Helmet } from "react-helmet-async";
+import { MonthRevenueCard } from "./month-revenue-card";
+import { MonthOrdersAmountCard } from "./month-orders-amount-card";
+import { DayOrdersAmountCard } from "./day-orders-amount-card";
+import { MonthCanceledOrdersAmountCard } from "./month-canceled-orders-amount-card";
 
 export function Dashboard() {
   return (
@@ -10,13 +12,10 @@ export function Dashboard() {
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
         <div className="grid grid-cols-4 gap-4">
-          <Card>
-            <CardHeader className="flex items-center justify-between pb-2">
-              <CardTitle className="text-base">Receita total (mês)</CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent></CardContent>
-          </Card>
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
         </div>
       </div>
     </>
