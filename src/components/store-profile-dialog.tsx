91680f22160b28743a4a2c11eb8199cc57faca31
leticea <nascimento.leticeah@gmail.com>
2024-03-29 18:35:22 -0300

    feat(store-profile-dialog.tsx): button styles

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,5 +1,30 @@
-import { DialogContent } from "./ui/dialog";
+import { Button } from "./ui/button";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
 
 export function StoreProfileDialog() {
-  return <DialogContent>Teste</DialogContent>;
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>
+          Atualize as informações do seu estabelecimento visíveis ao seu cliente
+        </DialogDescription>
+      </DialogHeader>
+
+      <DialogFooter>
+        <Button type="button" variant="ghost">
+          Cancelar
+        </Button>
+        <Button type="submit" variant="success">
+          Salvar
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  );
 }
