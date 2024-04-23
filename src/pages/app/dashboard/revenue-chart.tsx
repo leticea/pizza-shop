37b5a0273b0465c0f977fe4e2805679cd962d408
leticea <nascimento.leticeah@gmail.com>
2024-04-23 19:58:50 -0300

    feat(revenue-chart.tsx): formatting chart values ​​to display correctly

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -9,7 +9,7 @@ import {
 import { DateRangePicker } from "@/components/ui/date-range-picker";
 import { Label } from "@/components/ui/label";
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { DateRange } from "react-day-picker";
 import { subDays } from "date-fns";
 
@@ -22,6 +22,7 @@ import {
   CartesianGrid,
 } from "recharts";
 import colors from "tailwindcss/colors";
+import { Loader2 } from "lucide-react";
 
 export function RevenueChart() {
   const [dateRange, setDateRange] = useState<DateRange | undefined>({
@@ -37,6 +38,15 @@ export function RevenueChart() {
       }),
     queryKey: ["metrics", "daily-revenue-in-period", dateRange],
   });
+
+  const chartData = useMemo(() => {
+    return dailyRevenueInPeriod?.map((chartItem) => {
+      return {
+        date: chartItem.date,
+        receipt: chartItem.receipt / 100,
+      };
+    });
+  }, [dailyRevenueInPeriod]);
 
   return (
     <Card className="col-span-6">
@@ -54,9 +64,9 @@ export function RevenueChart() {
         </div>
       </CardHeader>
       <CardContent>
-        {dailyRevenueInPeriod && (
+        {chartData ? (
           <ResponsiveContainer width="100%" height={240}>
-            <LineChart data={dailyRevenueInPeriod} style={{ fontSize: 12 }}>
+            <LineChart data={chartData} style={{ fontSize: 12 }}>
               <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
 
               <YAxis
@@ -82,6 +92,10 @@ export function RevenueChart() {
               />
             </LineChart>
           </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </div>
         )}
       </CardContent>
     </Card>
