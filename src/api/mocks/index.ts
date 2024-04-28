fb332212a4eb5f2e46ef613df64eb10db652841f
leticea <nascimento.leticeah@gmail.com>
2024-04-28 13:22:14 -0300

    feat(get-day-orders-amount-mock.ts): creating mock

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,8 +2,13 @@ import { env } from "@/env";
 import { setupWorker } from "msw/browser";
 import { signInMock } from "./sign-in-mock";
 import { registerRestaurantMock } from "./register-restaurant-mock";
+import { getDayOrdersAmountMock } from "./get-day-orders-amont";
 
-export const worker = setupWorker(signInMock, registerRestaurantMock);
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+);
 
 export async function enableMSW() {
   if (env.MODE !== "test") {
