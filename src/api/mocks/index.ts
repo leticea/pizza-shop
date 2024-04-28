6d6904fcfcaf0ffec547986aabf2f5b34f29fbae
leticea <nascimento.leticeah@gmail.com>
2024-04-28 13:32:22 -0300

    creating dashboard mocks

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,12 +2,18 @@ import { env } from "@/env";
 import { setupWorker } from "msw/browser";
 import { signInMock } from "./sign-in-mock";
 import { registerRestaurantMock } from "./register-restaurant-mock";
-import { getDayOrdersAmountMock } from "./get-day-orders-amont";
+import { getDayOrdersAmountMock } from "./get-day-orders-amount";
+import { getMonthOrdersAmountMock } from "./get-month-orders-amount";
+import { getMonthCanceledOrdersAmountMock } from "./get-month-canceled-orders-amount";
+import { getMonthRevenueMock } from "./get-month-revenue";
 
 export const worker = setupWorker(
   signInMock,
   registerRestaurantMock,
   getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
 );
 
 export async function enableMSW() {
